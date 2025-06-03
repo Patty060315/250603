3fe5dcdef4283a66e95096ff344d90acd9a7e2fd
Patty060315 <413730176@o365.tku.edu.tw>
2025-06-03 14:51:16 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,7 +18,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(1200, 800); // 將畫布大小調整為 1200x800
+  createCanvas(1300, 850); // 將畫布大小調整為 1200x800
   // Detect video & load ML model
   video = createCapture(VIDEO, {flipped: true});
   video.hide();
